bcb55ecc84b4ed560a3635950570700082199118
Aruneet <87275550+Aruneet-11@users.noreply.github.com>
2025-04-06 12:01:37 +0530

    test

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
-export default function Singin() {
+export default function Counter() {
+  // Create a state variable for the counter
+  const [counter, setCounter] = useState(0);
+
+  // Function to increment the counter
+  const increment = () => setCounter(counter + 1);
+
+  // Function to decrement the counter
+  const decrement = () => setCounter(counter - 1);
+
   return (
-   <div>singing
-   </div>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
   );
 }
