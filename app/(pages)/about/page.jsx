b20443d01f54b9558dd02fbc164efab7379cf844
Aruneet <87275550+Aruneet-11@users.noreply.github.com>
2025-04-09 23:07:19 +0530

    Navbar

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -1,8 +1,13 @@
-import Image from "next/image";
+// import Navbar from "../components/navbar"
 
-export default function About() {
-  return (
-   <div>about folder
-   </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <main>
+//       <Navbar />
+//       <div className="container mx-auto p-4 mt-8">
+//         <h1 className="text-2xl font-bold">Welcome to RELOT</h1>
+//         <p className="mt-4">Your premium shopping destination</p>
+//       </div>
+//     </main>
+//   )
+// }
