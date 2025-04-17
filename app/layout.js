af40bd96fc407f1bccfb6f0df29621d0eeed1283
ayushhunt <ayushoo7hunt@gmail.com>
2025-04-18 00:36:59 +0530

    build fix

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,13 @@
 "use client"
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';import Navbar from "./components/navbar";
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
 import { usePathname } from 'next/navigation';
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // export const metadata = {
 //   title: "Create Next App",
@@ -33,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
        {showLayout && <Navbar />}
        <main> {children}</main>
