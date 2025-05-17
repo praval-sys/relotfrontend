8c990ce79352a1ec80427c74e5b4c95f7efda8de
Ayush Singh <ayushoo7hunt@gmail.com>
2025-05-17 20:50:05 +0530

    added docker compose and env

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://69.62.85.32:5000'; // Hardcoded API URL
 
 const api = axios.create({
-  baseURL: BASE_URL || 'http://localhost:3000/',
+  baseURL: process.env.API_URL || 'http://localhost:3000/',
   withCredentials: true,
 });
 
