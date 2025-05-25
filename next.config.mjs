4d5ac09da30b5c4529b13704d1263d4e861f8d50
Ayush Singh <ayushoo7hunt@gmail.com>
2025-05-25 20:32:55 +0530

    update next config to render the images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["relot.in","example.com","69.62.85.32"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '69.62.85.32',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'relot.in',
+      },
+    ],
   },
 };
 
